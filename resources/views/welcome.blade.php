ed064366a2a4eb56fb8013a02ea39f033f0a5696
Branick Weix <bdweix@gmail.com>
2017-06-05 18:42:06 -0500

    Titel update again
    Dfsjlf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Aryeo
+                    Welcome to Aryeo
                 </div>
 
                 <div class="links">
