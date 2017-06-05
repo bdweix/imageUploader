54cd664fc4c0e4adff2ff8a46bbf76c94d3063c6
Branick Weix <bdweix@gmail.com>
2017-06-05 18:40:40 -0500

    Name change
    fdsf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Aryeo
                 </div>
 
                 <div class="links">
