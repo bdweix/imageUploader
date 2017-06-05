9af79ee561626e7f54f3e4b09eb9f8b9a5192aa4
Branick Weix <bdweix@gmail.com>
2017-06-05 18:47:38 -0500

    Main menu update
    dsad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,12 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Aryeo
+                    Aryeo
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://agile-mesa-70912.herokuapp.com/upload">Upload</a>
+                    <a href="https://agile-mesa-70912.herokuapp.com/gallery">Gallery</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
