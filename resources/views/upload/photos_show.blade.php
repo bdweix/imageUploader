d2cf6a744571c725555268fe92f93a12f4ab9774
Thomas Kotopoulos <ktop@Toms-MBP.hsd1.ma.comcast.net>
2017-06-05 20:28:55 -0400

    Gallery Test
    gallery elements added

--- resources/views/upload/photos_show.blade.php
+++ resources/views/upload/photos_show.blade.php
@@ -1,6 +1,36 @@
-@foreach($urls as $url)
+<html>
+  <head>
+    <style>
+          div.gallery {
+              margin: 5px;
+              border: 1px solid #ccc;
+              float: left;
+              width: 180px;
+          }
 
-  <img src="{{$url->filename}}" alt="">
+          div.gallery:hover {
+              border: 1px solid #777;
+          }
 
+          div.gallery img {
+              width: 100%;
+              height: auto;
+          }
 
-@endforeach
+          div.desc {
+              padding: 15px;
+              text-align: center;
+          }
+    </style>
+  </head>
+  <body>
+    @foreach($urls as $url)
+        <div class="gallery">
+          <a target="_blank" href="fjords.jpg">
+            <img src="{{$url->filename}}" alt="Fjords" width="300" height="200">
+          </a>
+          <div class="desc">Add a description of the image here</div>
+        </div>
+    @endforeach
+  </body>
+</html>
